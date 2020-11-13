8a1bac81804e567460dc5b937d72f994e9915198
Tansique <63148306+tansique-17@users.noreply.github.com>
2020-11-13 20:44:32 +0530

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -8,7 +8,7 @@ import hashlib
 import signal
 
 os.system("clear")
-print """
+print ("""
 	  ___  ___ __ _________  ___ 
 	 / _ \/ -_) // / __/ _ \/ _ \ 
 	/_//_/\__/\_,_/_/  \___/_//_/
@@ -16,11 +16,11 @@ print """
         for queries: https://www.askthehackers.com
 
 
-"""
+""")
 
 def sigint_handler(signum, frame):
-    print '\n user interrupt ! shutting down'
-    print "[info] shutting down NEURON \n\n"
+    print ('\n user interrupt ! shutting down')
+    print ("[info] shutting down NEURON \n\n")
     sys.exit()	
     
 
@@ -54,7 +54,7 @@ def decrypt(secret,data):
 
 def chat_client():
     if(len(sys.argv) < 5) :
-        print 'Usage : python neuron.py <hostname> <port> <password> <nick_name>'
+        print ('Usage : python neuron.py <hostname> <port> <password> <nick_name>')
         sys.exit()
 
     host = sys.argv[1]
@@ -71,10 +71,10 @@ def chat_client():
         s.connect((host, port))
 
     except :
-        print "\033[91m"+'Unable to connect'+"\033[0m"
+        print ("\033[91m"+'Unable to connect'+"\033[0m")
         sys.exit()
 
-    print "Connected to remote host. You can start sending messages"
+    print ("Connected to remote host. You can start sending messages")
     sys.stdout.write("\033[34m"+'\n[Me :] '+ "\033[0m"); sys.stdout.flush()
 
     while 1:
@@ -87,7 +87,7 @@ def chat_client():
                 data = sock.recv(4096)
 
                 if not data :
-                    print "\033[91m"+"\nDisconnected from chat server"+"\033[0m"
+                    print ("\033[91m"+"\nDisconnected from chat server"+"\033[0m")
                     sys.exit()
                 else :
                     data = decrypt(key,data)
